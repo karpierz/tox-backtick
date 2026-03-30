c3bdd5a01070ac311bafd02cedb9e8336a3ff07b
Adam Karpierz <akarpierz@gmail.com>
2026-03-30 10:36:42 +0200

    2.3.0 (2026-03-30) ------------------ - Setup update and improvement.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
-# Copyright (c) 2023 Adam Karpierz
-# SPDX-License-Identifier: LGPL-2.1-or-later
+# Copyright (c) 1994 Adam Karpierz
+# SPDX-License-Identifier: Zlib
 
 __all__ = ('top_dir', 'test_dir')
 
